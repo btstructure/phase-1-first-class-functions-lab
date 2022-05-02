6d218cd5452b892a1651d4ad84ab4a2d2b2f22d2
Beyhan <beyhan.taylan@hotmail.com>
2022-05-01 20:01:00 -0400

    commit tree 8fcf5fe94ebefe4c4ce99246bdec68906de95e92

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+    
+}
+
+const returnLastTwoDrivers = function (selectingDrivers){
+    return selectingDrivers.slice(-2) //works backwards than a positive
+
+}
+
+const selectingDrivers = [ 
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+]
+
+function createFareMultiplier(int){
+    return function(fare){
+        return int * fare 
+    }
+}
+
+// const fareDoubler = {fare} => createFareMultiplier(num)
+
+const fareDoubler = createFareMultiplier(2)
+
+
+const fareTripler = createFareMultiplier(3)
+
+// function selectDifferentDrivers(drivers, funct){
+//     return funct(drivers);
+//     }
+
+
+const selectDifferentDrivers = (drivers, funct) => funct(drivers)
